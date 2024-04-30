faf56edf6d19f664debe110770332807eac8301a
Luanny <facilmigra@gmail.com>
2024-04-30 14:27:50 -0300

    ALT

--- app/controllers/pginicialController.js
+++ app/controllers/pginicialController.js
@@ -1,4 +1,46 @@
-const tarefasModel = require("../models/pginicial");
+const pginicialModel = require("../models/pginicial");
 const app = express()
 const pginicialController = {
-   
+  index: async (req, res) => {
+    try {
+      const tarefas = await pginicialModel.getTarefas();
+      res.render('index', { tarefas });
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Erro ao obter tarefas');
+    }
+  },
+ 
+  adicionarTarefa: async (req, res) => {
+    try {
+      const { tarefa, prazo, situacao } = req.body;
+      await pginicialModel.addTarefa(tarefa, prazo, situacao);
+      res.redirect('/');
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Erro ao adicionar tarefa');
+    }
+  },
+ 
+  alterarSituacao: async (req, res) => {
+    try {
+      const { id, situacao } = req.body;
+      await pginicialModel.updateSituacao(id, situacao);
+      res.redirect('/');
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Erro ao alterar situação da tarefa');
+    }
+  },
+ 
+  excluirTarefa: async (req, res) => {
+    try {
+      const { id } = req.params;
+      await pginicialModel.deleteTarefa(id);
+      res.redirect('/');
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Erro ao excluir tarefa');
+    }
+  }
+};
--- app/models/pginicial.js
+++ app/models/pginicial.js
@@ -1,66 +1,21 @@
 var pool = require("../../config/pool_conexoes");
+const tarefasModel = require('./tarefasModel');
 
-const tarefasModel = {
-    findAll: async () => {
-        try {
-            const [linhas] = await pool.query('SELECT * FROM tarefas WHERE status_tarefa = 1')
-            return linhas;
-        } catch (error) {
-            return error;
-        }
-    },
+// Listar todas as tarefas ativas
+tarefasModel.findAll().then(tarefas => {
+  console.log(tarefas);
+}).catch(error => {
+  console.error(error);
+});
 
-    findId: async (id) => {
-        try {
-            const [linhas,campos] = await pool.query('SELECT * FROM tarefas WHERE status_tarefa = 1 and id_tarefa = ?',[id] )
-            return linhas;
-        } catch (error) {
-            return error;
-        }
-    },
-
-   
-
-    create: async (dadosForm) => {
-        try {
-            const [linhas, campos] = await pool.query('INSERT INTO tarefas SET ?', [dadosForm])
-            console.log(linhas);
-            console.log(campos);
-            return linhas;
-        } catch (error) {
-            console.log(error);
-            return null;
-        }  
-    },
-
-    update: async (dadosForm, id) => {
-        try {
-            const [linhas] = await pool.query('UPDATE tarefas SET ? WHERE id_tarefa = ?', [dadosForm, id])
-            return linhas;
-        } catch (error) {
-            return error;
-        }  
-    },
-
-    delete: async (id) => {
-        try {
-            const [linhas] = await pool.query('UPDATE tarefas SET status_tarefa = 0  WHERE id_tarefa = ?', [id])
-            return linhas;
-        } catch (error) {
-            return error;
-        }  
-    },
-
-    sistuacaoTarefa: async (situacao, id) => {
-        try {
-            const [linhas] = await pool.query('UPDATE tarefas SET situacao_tarefa = ? WHERE id_tarefa = ?', [situacao, id])
-            return linhas;
-        } catch (error) {
-            return error;
-        }  
-    }
-  
+// Criar uma nova tarefa
+const dadosForm = {
+  nome_tarefa: 'Nova tarefa',
+  descricao_tarefa: 'Descrição da tarefa',
+  //...
 };
-    
-
-module.exports = tarefasModel
+tarefasModel.create(dadosForm).then(tarefa => {
+  console.log(tarefa);
+}).catch(error => {
+  console.error(error);
+});
