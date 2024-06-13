d6c819f46a93871e0d3a2fbbb27e189edd42dc23
luanny <facilmigra@gmail.com>
2024-06-13 18:59:14 -0300

    alt

--- app/routes/router.js
+++ app/routes/router.js
@@ -1,15 +1,55 @@
-const express = require("express");
-const router = express.Router();
+var express = require("express");
+var router = express.Router();
 const pginicialController = require("../controllers/pginicialController");
 
+router.get("/", function (req, res) {
+  res.render("pages/pginicial", {pagina:"home", logado:null});
+});
+
 router.get("/login", function (req, res) {
-  res.render("pages/template-home", {pagina:"login", logado:null});
+  res.render("pages/login", {pagina:"login", logado:null});
 });
-router.get("/cadastro", function (req, res) {
-  res.render("pages/template-home", {pagina:"cadastro", logado:null});
+
+router.get("/logado", function (req, res) {
+  res.render("pages/logado", {pagina:"logado", logado:"logado"});
 });
-router.get("/perfil", function (req, res) {
-  res.render("pages/template-home", {pagina:"perfil", logado:"logado"});
+
+router.get("/logado", function (req, res) {
+  res.render("pages/logado", {pagina:"logado", logado:"logado"});
+});
+
+
+router.get("/filtro", function (req, res) {
+  res.render("pages/filtro", {pagina:"filtro", logado:null});
+});
+router.get("/empregos", function (req, res) {
+  res.render("pages/empregos", {pagina:"empregos", logado: null });
+});
+router.get("/criar", function (req, res) {
+  res.render("pages/criar", {pagina:"cadastro", logado:"null"});
+});
+
+router.get("/", function (req, res) {
+  res.render("pages/adm/template-adm",{pagina:"index"});
+});
+router.get("/adm-cliente", function (req, res) {
+  res.render("pages/adm/template-adm",{pagina:"cliente/index"});
+});
+
+router.get("/adm-cliente-novo", function (req, res) {
+  res.render("pages/adm/template-adm",{pagina:"cliente/create"});
+});
+
+router.get("/adm-cliente-edit", function (req, res) {
+  res.render("pages/adm/template-adm",{pagina:"cliente/edit"});
+});
+
+router.get("/adm-cliente-list", function (req, res) {
+  res.render("pages/adm/template-adm",{pagina:"cliente/detalhes"});
+});
+
+router.get("/adm-cliente-del", function (req, res) {
+  res.render("pages/adm/template-adm",{pagina:"cliente/delete"});
 });
 
 
