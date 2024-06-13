752fb79aec685b5f16cdf117cf4db66c3c3c1013
luanny <facilmigra@gmail.com>
2024-06-13 20:38:49 -0300

    alt

--- app/routes/router.js
+++ app/routes/router.js
@@ -22,35 +22,25 @@ router.get("/logado", function (req, res) {
 router.get("/filtro", function (req, res) {
   res.render("pages/filtro", {pagina:"filtro", logado:null});
 });
+
 router.get("/empregos", function (req, res) {
   res.render("pages/empregos", {pagina:"empregos", logado: null });
 });
+
 router.get("/criar", function (req, res) {
   res.render("pages/criar", {pagina:"cadastro", logado:"null"});
 });
 
-router.get("/", function (req, res) {
-  res.render("pages/adm/template-adm",{pagina:"index"});
-});
-router.get("/adm-cliente", function (req, res) {
-  res.render("pages/adm/template-adm",{pagina:"cliente/index"});
+router.get("/anuncio", function (req, res) {
+  res.render("pages/anuncio",{pagina:"anuncio casas", logado: "null"});
 });
 
-router.get("/adm-cliente-novo", function (req, res) {
-  res.render("pages/adm/template-adm",{pagina:"cliente/create"});
-});
 
-router.get("/adm-cliente-edit", function (req, res) {
-  res.render("pages/adm/template-adm",{pagina:"cliente/edit"});
-});
 
-router.get("/adm-cliente-list", function (req, res) {
-  res.render("pages/adm/template-adm",{pagina:"cliente/detalhes"});
-});
 
-router.get("/adm-cliente-del", function (req, res) {
-  res.render("pages/adm/template-adm",{pagina:"cliente/delete"});
-});
+
+
+
 
 
 
