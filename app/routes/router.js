db4c2be62fdeb353c210bdf027b277f99d4fe739
jhennyfer <facilmigra@gmail.com>
2024-06-26 13:40:09 -0300

    onbouo

--- app/routes/router.js
+++ app/routes/router.js
@@ -50,16 +50,36 @@ router.get("/login", function (req, res) {
   res.render("pages/login", {pagina:"logado", logado:"logado"});
 });
 
-router.get("/logado", function (req, res) {
-  res.render("pages/logado", {pagina:"logado", logado:"logado"});
+router.get("/casas", function (req, res) {
+  res.render("pages/casas", {pagina:"casas", logado:"casas"});
 });
 
 router.get("/filtro", function (req, res) {
   res.render("pages/filtro", {pagina:"filtro", logado:null});
 });
 
-router.get("/empregos", function (req, res) {
-  res.render("pages/empregos", {pagina:"empregos", logado: null });
+router.get("/emprego", function (req, res) {
+  res.render("pages/emprego", {pagina:"emprego", logado: null });
+});
+
+router.get("/anuncios", function (req, res) {
+  res.render("pages/anuncios", {pagina:"anuncios", logado:"anuncios"});
+});
+
+router.get("/detalhes", function (req, res) {
+  res.render("pages/detalhes", {pagina:"detalhes", logado:"detalhes"});
+});
+
+router.get("/favoritos", function (req, res) {
+  res.render("pages/favortitos", {pagina:"favoritos", logado:"favoritos"});
+});
+
+router.get("/filtrocasas", function (req, res) {
+  res.render("pages/filtrocasas", {pagina:"filtrocasas", logado:"filtrocasas"});
+});
+
+router.get("/filtrovagas", function (req, res) {
+  res.render("pages/filtrovagas", {pagina:"filtrovagas", logado:"filtrovagas"});
 });
 
 router.get("/cadastro", function (req, res) {
