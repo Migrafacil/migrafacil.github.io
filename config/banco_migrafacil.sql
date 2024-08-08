95fff6c42919a96381410ba76de3818c891a48b1
Kauã <facilmigra@gmail.com>
2024-08-08 11:49:25 -0300

    hfdhz

--- config/banco_migrafacil.sql
+++ config/banco_migrafacil.sql
@@ -299,3 +299,256 @@ SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2024-06-25 14:21:27
+
+-- MySQL Workbench Forward Engineering
+ 
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+ 
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema bpysjgvodzhdqziw3zwg
+-- -----------------------------------------------------
+ 
+-- -----------------------------------------------------
+-- Schema bpysjgvodzhdqziw3zwg
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `bpysjgvodzhdqziw3zwg` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `bpysjgvodzhdqziw3zwg` ;
+ 
+-- -----------------------------------------------------
+-- Table `bpysjgvodzhdqziw3zwg`.`cliente`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `bpysjgvodzhdqziw3zwg`.`cliente` (
+  `ClienteID` INT NOT NULL,
+  `Nome_Cliente` VARCHAR(255) NOT NULL,
+  `Endereco_Cliente` VARCHAR(255) NOT NULL,
+  `Logradouro_Cliente` VARCHAR(100) NOT NULL,
+  `Bairro_Cliente` VARCHAR(255) NOT NULL,
+  `Cidade_Cliente` VARCHAR(255) NOT NULL,
+  `UF_Cliente` CHAR(2) NOT NULL,
+  `CEP_Cliente` CHAR(8) NOT NULL,
+  `Email_Cliente` VARCHAR(255) NOT NULL,
+  `Celular_Cliente` VARCHAR(15) NOT NULL,
+  `Contato_Cliente` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`ClienteID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+ 
+ 
+-- -----------------------------------------------------
+-- Table `bpysjgvodzhdqziw3zwg`.`assinatura`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `bpysjgvodzhdqziw3zwg`.`assinatura` (
+  `AssinaturaID` INT NOT NULL,
+  `Logradouro_Assinatura` VARCHAR(100) NOT NULL,
+  `Bairro_Assinatura` VARCHAR(255) NOT NULL,
+  `Cidade_Assinatura` VARCHAR(255) NOT NULL,
+  `UF_Assinatura` CHAR(2) NOT NULL,
+  `CEP_Assinatura` CHAR(8) NOT NULL,
+  `IdAssinatura` INT NOT NULL,
+  PRIMARY KEY (`AssinaturaID`),
+  INDEX `FK_Assinatura_IdAssinatura` (`IdAssinatura` ASC) VISIBLE,
+  CONSTRAINT `FK_Assinatura_IdAssinatura`
+    FOREIGN KEY (`IdAssinatura`)
+    REFERENCES `bpysjgvodzhdqziw3zwg`.`cliente` (`ClienteID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+ 
+ 
+-- -----------------------------------------------------
+-- Table `bpysjgvodzhdqziw3zwg`.`imovel`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `bpysjgvodzhdqziw3zwg`.`imovel` (
+  `ImovelID` INT NOT NULL,
+  `Endereco_Imovel` VARCHAR(255) NOT NULL,
+  `Logradouro_Imovel` VARCHAR(100) NOT NULL,
+  `Bairro_Imovel` VARCHAR(255) NOT NULL,
+  `Cidade_Imovel` VARCHAR(255) NOT NULL,
+  `UF_Imovel` CHAR(2) NOT NULL,
+  `CEP_Imovel` CHAR(8) NOT NULL,
+  `IdImoveis` INT NOT NULL,
+  PRIMARY KEY (`ImovelID`),
+  INDEX `FK_Imovel_IdImoveis` (`IdImoveis` ASC) VISIBLE,
+  CONSTRAINT `FK_Imovel_IdImoveis`
+    FOREIGN KEY (`IdImoveis`)
+    REFERENCES `bpysjgvodzhdqziw3zwg`.`cliente` (`ClienteID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+ 
+ 
+-- -----------------------------------------------------
+-- Table `bpysjgvodzhdqziw3zwg`.`vaga`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `bpysjgvodzhdqziw3zwg`.`vaga` (
+  `VagaID` INT NOT NULL,
+  `DescricaoVaga` TEXT NOT NULL,
+  `CargaHoraria` VARCHAR(255) NOT NULL,
+  `IdVagas` INT NOT NULL,
+  PRIMARY KEY (`VagaID`),
+  INDEX `FK_Vaga_IdVagas` (`IdVagas` ASC) VISIBLE,
+  CONSTRAINT `FK_Vaga_IdVagas`
+    FOREIGN KEY (`IdVagas`)
+    REFERENCES `bpysjgvodzhdqziw3zwg`.`imovel` (`ImovelID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+ 
+ 
+-- -----------------------------------------------------
+-- Table `bpysjgvodzhdqziw3zwg`.`candidato`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `bpysjgvodzhdqziw3zwg`.`candidato` (
+  `CandidatoID` INT NOT NULL,
+  `Nome_Candidato` VARCHAR(255) NOT NULL,
+  `CPF_Candidato` VARCHAR(11) NOT NULL,
+  `Data_Nascimento` DATE NOT NULL,
+  `Endereco_Candidato` VARCHAR(255) NOT NULL,
+  `Logradouro_Candidato` VARCHAR(255) NOT NULL,
+  `Bairro_Candidato` VARCHAR(255) NOT NULL,
+  `Cidade_Candidato` VARCHAR(255) NOT NULL,
+  `UF_Candidato` CHAR(2) NOT NULL,
+  `CEP_Candidato` CHAR(8) NOT NULL,
+  `Email_Candidato` VARCHAR(255) NOT NULL,
+  `Celular_Candidato` VARCHAR(15) NOT NULL,
+  `Contato_Candidato` VARCHAR(255) NOT NULL,
+  `vaga_VagaID` INT NOT NULL,
+  PRIMARY KEY (`CandidatoID`),
+  INDEX `fk_candidato_vaga1_idx` (`vaga_VagaID` ASC) VISIBLE,
+  CONSTRAINT `fk_candidato_vaga1`
+    FOREIGN KEY (`vaga_VagaID`)
+    REFERENCES `bpysjgvodzhdqziw3zwg`.`vaga` (`VagaID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+ 
+ 
+-- -----------------------------------------------------
+-- Table `bpysjgvodzhdqziw3zwg`.`empresa`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `bpysjgvodzhdqziw3zwg`.`empresa` (
+  `EmpresaID` INT NOT NULL,
+  `RazaoSocial` VARCHAR(255) NOT NULL,
+  `EnderecoEmprego` VARCHAR(255) NOT NULL,
+  `LogradouroEmprego` VARCHAR(100) NOT NULL,
+  `BairroEmprego` VARCHAR(255) NOT NULL,
+  `CidadeEmprego` VARCHAR(255) NOT NULL,
+  `UFEmprego` CHAR(2) NOT NULL,
+  `CEPEmprego` CHAR(8) NOT NULL,
+  `CNPJEmpresa` CHAR(14) NOT NULL,
+  PRIMARY KEY (`EmpresaID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+ 
+ 
+-- -----------------------------------------------------
+-- Table `bpysjgvodzhdqziw3zwg`.`cargo`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `bpysjgvodzhdqziw3zwg`.`cargo` (
+  `CargoID` INT NOT NULL,
+  `NomeCargo` VARCHAR(255) NOT NULL,
+  `EmpresaID` INT NOT NULL,
+  PRIMARY KEY (`CargoID`),
+  INDEX `FK_Cargo_Empresa` (`EmpresaID` ASC) VISIBLE,
+  CONSTRAINT `FK_Cargo_Empresa`
+    FOREIGN KEY (`EmpresaID`)
+    REFERENCES `bpysjgvodzhdqziw3zwg`.`empresa` (`EmpresaID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+ 
+ 
+-- -----------------------------------------------------
+-- Table `bpysjgvodzhdqziw3zwg`.`pagamento`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `bpysjgvodzhdqziw3zwg`.`pagamento` (
+  `PagamentoID` INT NOT NULL,
+  `DataPagamento` DATE NOT NULL,
+  `ValorPago` DECIMAL(10,2) NOT NULL,
+  `IdPagamento` INT NOT NULL,
+  PRIMARY KEY (`PagamentoID`),
+  INDEX `FK_Pagamento_IdPagamento` (`IdPagamento` ASC) VISIBLE,
+  CONSTRAINT `FK_Pagamento_IdPagamento`
+    FOREIGN KEY (`IdPagamento`)
+    REFERENCES `bpysjgvodzhdqziw3zwg`.`assinatura` (`AssinaturaID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+ 
+ 
+-- -----------------------------------------------------
+-- Table `bpysjgvodzhdqziw3zwg`.`alugar`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `bpysjgvodzhdqziw3zwg`.`alugar` (
+  `idalugar` INT NOT NULL,
+  `nome_alugar` VARCHAR(255) NULL,
+  `cnpj_alugar` CHAR(14) NULL,
+  `cpf_alugar` CHAR(8) NULL,
+  `email_alugar` VARCHAR(255) NULL,
+  `celular_alugar` VARCHAR(14) NULL,
+  `enderco_alugar` VARCHAR(255) NULL,
+  `logradouro_alugar` VARCHAR(100) NULL,
+  `bairro_alugar` VARCHAR(255) NULL,
+  `cidade_alugar` VARCHAR(255) NULL,
+  `uf_alugar` CHAR(2) NULL,
+  `cep_alugar` CHAR(8) NULL,
+  PRIMARY KEY (`idalugar`))
+ENGINE = InnoDB;
+ 
+ 
+-- -----------------------------------------------------
+-- Table `bpysjgvodzhdqziw3zwg`.`alugar_has_cliente`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `bpysjgvodzhdqziw3zwg`.`alugar_has_cliente` (
+  `alugar_idalugar` INT NOT NULL,
+  `cliente_ClienteID` INT NOT NULL,
+  PRIMARY KEY (`alugar_idalugar`, `cliente_ClienteID`),
+  INDEX `fk_alugar_has_cliente_cliente1_idx` (`cliente_ClienteID` ASC) VISIBLE,
+  INDEX `fk_alugar_has_cliente_alugar1_idx` (`alugar_idalugar` ASC) VISIBLE,
+  CONSTRAINT `fk_alugar_has_cliente_alugar1`
+    FOREIGN KEY (`alugar_idalugar`)
+    REFERENCES `bpysjgvodzhdqziw3zwg`.`alugar` (`idalugar`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_alugar_has_cliente_cliente1`
+    FOREIGN KEY (`cliente_ClienteID`)
+    REFERENCES `bpysjgvodzhdqziw3zwg`.`cliente` (`ClienteID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+ 
+ 
+-- -----------------------------------------------------
+-- Table `bpysjgvodzhdqziw3zwg`.`alugar_has_imovel`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `bpysjgvodzhdqziw3zwg`.`alugar_has_imovel` (
+  `alugar_idalugar` INT NOT NULL,
+  `imovel_ImovelID` INT NOT NULL,
+  PRIMARY KEY (`alugar_idalugar`, `imovel_ImovelID`),
+  INDEX `fk_alugar_has_imovel_imovel1_idx` (`imovel_ImovelID` ASC) VISIBLE,
+  INDEX `fk_alugar_has_imovel_alugar1_idx` (`alugar_idalugar` ASC) VISIBLE,
+  CONSTRAINT `fk_alugar_has_imovel_alugar1`
+    FOREIGN KEY (`alugar_idalugar`)
+    REFERENCES `bpysjgvodzhdqziw3zwg`.`alugar` (`idalugar`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_alugar_has_imovel_imovel1`
+    FOREIGN KEY (`imovel_ImovelID`)
+    REFERENCES `bpysjgvodzhdqziw3zwg`.`imovel` (`ImovelID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+ 
+ 
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
