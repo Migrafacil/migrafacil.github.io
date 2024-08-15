85abfde8d15bf02a9bffca02e10363f7d153e6a2
luanny <facilmigra@gmail.com>
2024-08-15 11:48:50 -0300

    alt

--- app/routes/router.js
+++ app/routes/router.js
@@ -49,6 +49,24 @@ router.post(
   }
 )
 
+router.get(
+  "/perfil",
+  verificarUsuAutorizado([1, 2, 3], "pages/restrito"),
+  async function (req, res) {
+    usuarioController.mostrarPerfil(req, res);
+  }
+);
+
+router.post(
+  "/perfil",
+  uploadFile("imagem-perfil_usu"),
+  usuarioController.regrasValidacaoPerfil,
+  verificarUsuAutorizado([1, 2, 3], "pages/restrito"),
+  async function (req, res) {
+    usuarioController.gravarPerfil(req, res);
+  }
+);
+
 router.get("/logado", function (req, res) {
   res.render("pages/logado", {pagina:"logado", logado:"logado"});
 });
