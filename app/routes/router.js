c47280a66a6098773983967a630dbfc60ee73169
Your Name <you@example.com>
2024-09-20 11:13:49 -0300

    jhennys

--- app/routes/router.js
+++ app/routes/router.js
@@ -165,5 +165,88 @@ router.get("/casas", function (req, res) {
   res.render("pages/casas",{pagina:"anuncios casas", logado: "null"});
 });
 
+var express = require("express");
+var router = express.Router();
+
+const {
+  verificarUsuAutenticado,
+  limparSessao,
+  gravarUsuAutenticado,
+  verificarUsuAutorizado,
+} = require("../models/autenticador_middleware");
+
+const usuarioController = require("../controllers/usuarioController");
+
+const uploadFile = require("../util/uploader")("./app/public/imagem/perfil/");
+// const uploadFile = require("../util/uploader")();
+
+router.get(
+  "/perfil",
+  verificarUsuAutorizado([1, 2, 3], "pages/restrito"),
+  async function (req, res) {
+    usuarioController.mostrarPerfil(req, res);
+  }
+);
+
+router.post(
+  "/perfil",
+  uploadFile("imagem-perfil_usu"),
+  usuarioController.regrasValidacaoPerfil,
+  verificarUsuAutorizado([1, 2, 3], "pages/restrito"),
+  async function (req, res) {
+    usuarioController.gravarPerfil(req, res);
+  }
+);
+
+
+router.get("/", verificarUsuAutenticado, function (req, res) {
+  res.render("pages/index", {
+    autenticado: req.session.autenticado,
+    login: req.session.logado,
+  });
+});
+
+router.get("/sair", limparSessao, function (req, res) {
+  res.redirect("/");
+});
+
+router.get("/login", function (req, res) {
+  res.render("pages/login", { listaErros: null, dadosNotificacao: null });
+});
+
+router.post(
+  "/login",
+  usuarioController.regrasValidacaoFormLogin,
+  gravarUsuAutenticado,
+  function (req, res) {
+    usuarioController.logar(req, res);
+  }
+);
+
+router.get("/cadastro", function (req, res) {
+  res.render("pages/cadastro", {
+    listaErros: null,
+    dadosNotificacao: null,
+    valores: { nome_usu: "", nomeusu_usu: "", email_usu: "", senha_usu: "" },
+  });
+});
+
+router.post(
+  "/cadastro",
+  usuarioController.regrasValidacaoFormCad,
+  async function (req, res) {
+    usuarioController.cadastrar(req, res);
+  }
+);
+
+router.get(
+  "/adm",
+  verificarUsuAutenticado,
+  verificarUsuAutorizado([2, 3], "pages/restrito"),
+  function (req, res) {
+    res.render("pages/adm", req.session.autenticado);
+  }
+);
+
 
 module.exports = router
